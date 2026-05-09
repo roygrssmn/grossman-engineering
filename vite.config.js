3820503eb791140984ef6bf9756a38052d6d50c2
Roy Grossman <roy.grossman@wooga.net>
2026-05-09 14:53:10 +0200

    Fixed Cloudflare issues, updated also the needed dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-   export default defineConfig({
-     plugins: [
-       tailwindcss(),
-       react()
-     ]
-   })
+import { cloudflare } from "@cloudflare/vite-plugin";
+
+export default defineConfig({
+  plugins: [tailwindcss(), react(), cloudflare()]
+})
